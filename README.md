38fdb4bf4ecfeba4b7748455dbae837c2ddce5b6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-01-11 15:17:31 +0100

    move into separate repository

---
+++ README.md
@@ -0,0 +1,6 @@
+[![website](https://img.shields.io/badge/website-direape.solsort.com-blue.svg)](https://direape.solsort.com/)
+[![github](https://img.shields.io/badge/github-solsort/direape-blue.svg)](https://github.com/solsort/direape)
+[![travis](https://img.shields.io/travis/solsort/direape.svg)](https://travis-ci.org/solsort/direape)
+[![npm](https://img.shields.io/npm/v/direape.svg)](https://www.npmjs.com/package/direape)
+
+# <img src=https://direape.solsort.com/icon.png width=64 height=64> DireApe - Distributed Reactive App Environment
---
+++ main.js
@@ -0,0 +1,123 @@
+var immutable = require('immutable');
+var direape = exports;
+
+// # Generic utility functions
+//
+// Should probably be replaced with better general module
+//
+function randomString() {
+  return Math.random().toString(32).slice(2);
+}
+function nextTick(f) {
+  setTimeout(f, 0);
+}
+function slice(a, start, end) {
+  return Array.prototype.slice.call(a, start, end);
+}
+function warn() {
+  console.log.apply(console, ['warn'].concat(slice(arguments)));
+}
+
+// # Internal methods
+var pid = "PID" + randomString() + randomString() + randomString();
+var state = new immutable.Map().set(pid, new immutable.Map());
+var prevState = state;
+var handlers = {};
+var messageQueue = [];
+var scheduled = false;
+var reactions = {};
+var transports = {};
+transports[pid] = o => {
+  var mbox = o.dst.slice(0, o.dst.lastIndexOf('@'));
+  if(handlers[mbox]) {
+    state = handlers[mbox].apply(o, [state].concat(o.data)) || state;
+  } else {
+    warn('missing handler for ', pid.length, o, mbox);
+  }
+}
+transports['*'] = o => self.postMessage(o);
+self.onmessage = o => {
+  //console.log('onmessage', o)
+  _dispatchAsync(o.data);
+}
+function _dispatchAsync(o) {
+   messageQueue.push(o);
+   _dispatchAll(); 
+};
+function _dispatchSync(o) {
+  //console.log(pid.slice(0,7), o);
+  o.dst = (o.dst || '').includes('@') ? o.dst : o.dst + '@' + pid;
+  var f = transports[o.dst.slice(o.dst.lastIndexOf('@') + 1)];
+  (f || transports['*'])(o);
+}
+
+function _dispatchAll() {
+  if(scheduled) {
+    return;
+  }
+  scheduled = true;
+  nextTick(function() {
+    scheduled = false;
+    var messages = messageQueue;
+    messageQueue = [];
+    for(var i = 0; i < messages.length; ++i) {
+      try {
+      _dispatchSync(messages[i])
+      } catch(e) {
+        warn('error during dispatch:', e);
+      }
+    }
+
+    if(!prevState.equals(state)) {
+      //console.log('reaction needed');  
+      for(var k in reactions) {
+        try {
+          reactions[k]();
+      } catch(e) {
+        warn('error during reaction:', e);
+      }
+      }
+      prevState = state;
+    } else {
+      //console.log('reaction unneeded');  
+    }
+  });
+}
+
+// # API
+direape.pid = pid;
+direape._transports = transports;
+direape.handle = (eventType, f) => { handlers[eventType] = f; }
+direape.call = dst => _dispatchAsync({dst: dst, data: slice(arguments, 1)}); 
+direape.dispatchAsync = _dispatchAsync;
+direape.dispatchSync = (o) => { _dispatchSync(o); _dispatchAll(); };
+direape.getIn = (ks, defaultValue) => state.getIn(ks, defaultValue);
+direape.reaction = (name, f) => { reactions[name] = f; }
+
+// # Built-in event handlers
+direape.handle('reun.run', (state, code, uri) => {
+  require('reun').run(code, uri);
+});
+direape.handle('direape.getIn', (state, ks, mbox) => {
+  direape.dispatchAsync({dst: mbox, data: [direape.getIn(ks)]});
+});
+direape.handle('direape.setIn', (state, ks, value) => state.setIn(ks,value)); 
+
+var subscriptions = new Set();
+direape.handle('direape.subscribe', function(state, path, dst) {
+  subscriptions.add([path, dst]);
+});
+direape.handle('direape.unsubscribe', function(state, path, dst) {
+  subscriptions.delete([path, dst]);
+});
+direape.reaction('direape.subscriptions', function() {
+  for(var v of subscriptions) {
+    direape.dispatchAsync({dst: v[1], data:[direape.getIn(v[0])]});
+  }
+});
+
+try {
+  postMessage({dst: 'direape.workerReady', data: [pid]});
+} catch(e) {
+}
+
