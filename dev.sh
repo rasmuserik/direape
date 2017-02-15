d4afa624f3be800c6bcbd19a1abd1d18caeb6167
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-15 11:00:01 +0100

    minification

--- dev.sh
+++ dev.sh
@@ -1,18 +1,18 @@
-if [ ! -e node_modules/.bin/live-server ]; then npm install live-server eslint; fi
-cat direape.js | sed -e 's/^/    /' | sed -e 's/^ *[/][/] \?//' > README.md
-./node_modules/.bin/eslint *.js;
+if [ ! -e node_modules/.bin/live-server ]; then npm install --save-dev live-server eslint uglify-js-harmony; fi
 
 ./node_modules/.bin/live-server --no-browser &
 echo $! > .pid-live-server
 
-export NODE_TLS_REJECT_UNAUTHORIZED=0
-npm install --dev
+(sleep 0.5; touch direape.js) &
 while inotifywait -e modify,close_write,move_self -q *.js
 do 
   kill `cat .pid`
   sleep 0.1
   DIREAPE_DEV=true node direape.js test server $@ &
   echo $! > .pid
+  cat direape.js | sed -e 's/^/    /' | sed -e 's/^ *[/][/] \?//' > README.md
+  ./node_modules/.bin/eslint direape.js &
+  ./node_modules/.bin/uglifyjs -c 'pure_funcs=["da.test"]' < direape.js > direape.min.js 2> /dev/zero &
   sleep 3
 done
 
